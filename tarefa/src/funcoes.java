661374adbcb1e1c715669798e14cce9169eb81e8
matheusniak <233319463+matheusniak@users.noreply.github.com>
2026-03-11 22:39:51 +0000

    primeiro commit (muitos erros e não entendi o motivo)

---
+++ tarefa/src/funcoes.java
@@ -0,0 +1,77 @@
+public class Main {
+    public static int f1(int n) {
+        int r = 0;
+        int op = 0;
+        for (int i = 1; i < n; i++) {
+            r = r + 1;
+            op = op + 1;
+        }
+        return op;
+    }
+
+    public static int f2(int n) {
+        int r = 0;
+        int op = 0;
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                op = op + 1;
+                r = r + 2;
+            }
+        }
+        return op;
+    }
+
+    //public static int f3(int n)
+
+int f3(n)
+cont=0
+for (i=1; i<n; i++)
+for (j=1; j<n; j++)
+print i*j
+cont++
+return cont
+
+    //public static int f4(int n)
+    
+    int f4(n)
+r=0
+for (i=1; i<n; i++)
+for (j=i; j<2*i; j++)
+for (k=i; k<j; k++)
+r = r + 1
+return r
+
+    //public static int f5(int n)
+
+int f5(n)
+r=0
+for (i=1; i<n; i++)
+for (j=i; j<i+3; j++)
+for (k=i; k<j; k++)
+r = r + 1
+return r
+
+    //public static int f6(int n)
+
+int f6(n)
+if (n==0)
+return 1
+else
+return f6(n-1) + f6(n-1)
+
+    public static void main(String[] args) {
+        int op = 0;
+        System.out.println("N    |#OP");
+
+        //para cada funcao descomente a linha para chamar a funcao desejada
+        for (int n = 0; n <= 100; n+=10) {
+            op = f1(n);
+            //op = f2(n);
+            //op = f2(n);
+            //op = f2(n);
+            //op = f2(n);
+
+            System.out.println(n + "    |" + op);
+        }
+    }
+}
